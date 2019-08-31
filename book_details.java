fcbae2b82ff179199fa0b5c9f24f7ea5b231c522
prathibha27 <42810644+prathibha27@users.noreply.github.com>
2019-08-31 13:03:48 +0530

    Update book_details.java

--- book_details.java
+++ book_details.java
@@ -1,63 +1,58 @@
 import java.util.*;
-public class book
+import java.lang.Math;
+
+class book
 {
-	public String name;
-	public String author;
-	public double price;
-	public int num_pages;
+	public static String name;
+	public static String author;
+	public static double price;
+	public static int num_pages;
        book()
       {
-	name="please enter the bookname";
-	author="please enter the author name";
-        price = 0.00;
- 	num_pages=0;
+    	name="xx";
+	    author="xx";
+      price = 0.00;
+ 	     num_pages=0;
       }
      book(String  n , String a , double p , int n_p)
-    {
-  	name = n ;
-        author= a;
-	price = p;
-	num_pages= n_p;
-    } 
+      {
+     	name = n ;
+      author= a;
+	   price = p;
+	   num_pages= n_p;
+     } 
 
    public void set()
-{
+   {
   Scanner s = new Scanner(System.in);
   System.out.println("enter the name of the book");
- name = s.nextLine();
+ name = s.next();
  System.out.println("enter the name of the author");
- author = s.nextLine();
+ author = s.next();
  System.out.println("enter the price of  the book");
  price =s.nextDouble();
  System.out.println("how many pages does the book contain ?");
  num_pages=s.nextInt();
- book b1 = new  book( name , author , price , num_pages);
 }
 
 public void get()
-{System.out.println("Name :"+name);
-System.out.println("Author :"+author);
+{
+  System.out.println("Name :"+name);
+ System.out.println("Author :"+author);
 System.out.println("Price :"+price);
 System.out.println("Number of pages :"+num_pages);
 
 }
-public static void main(String args[])
-{ int i,h;
- 
- System.out.println("enter the number of books");
- Scanner s = new Scanner(System.in);
-  h = s.nextInt();
-  book b1 = new book(); 
- for (i=0;i<=h;i++)
- {
-    
-   b1.set();
-   b1.get();
- 
- } 
- 
-} 
 }
 
+class book_demo1
+{ 
+   public static void main(String args[])
+      {
  
+      book b1 = new  book("Prathibha","R",999,100);
+      b1.set();
+      b1.get();
+      }
+}
  
